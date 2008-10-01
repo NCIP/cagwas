7e013b139fde15f0bbc2105b4f75ab169c4d9b3c
saksass <saksass@7a08ea27-3732-45e7-be6b-e8af83fd36ee>
2008-10-01 20:32:19 +0000

    Db upgrade

---
+++ software/cagwas/db/db-upgrade/mysql/generisize-081001.sql
@@ -0,0 +1,29 @@
+alter table odds_ratio modify column `heterozygote` decimal(25,12) default NULL;
+alter table odds_ratio modify column`homozygote` decimal(12,6) default NULL;
+
+
+alter table odds_ratio add column(
+	`heterozygote_lower_cb` decimal(12,6) default NULL,
+	`heterozygote_upper_cb` decimal(12,6) default NULL,
+	`heterozygote_std_err` decimal(12,6) default NULL,
+	`homozygote_lower_cb` decimal(12,6) default NULL,
+	`homozygote_upper_cb` decimal(12,6) default NULL,
+	`homozygote_std_err` decimal(12,6) default NULL
+);
+
+alter table snp_analysis_lu modify column  `analysis_method_desc` varchar(1000) default NULL;
+alter table snp_analysis_lu modify column  `analysis_method_name` varchar(100) default NULL;
+
+alter table snp_analysis_lu add column(
+	`def_option_order` decimal(6,3) default NULL
+);
+
+
+alter table snp_association_analysis modify column  `asso_analysis_code` varchar(20) default NULL;
+alter table snp_association_analysis modify column  `asso_analysis_name` varchar(500) default NULL;
+
+alter table snp_association_analysis add column(
+	`number_of_categories` decimal(22,0) default NULL
+);
+
+
