59b1a2b839174666541c31f8712be7cac719b954
narram <narram@7a08ea27-3732-45e7-be6b-e8af83fd36ee>
2009-10-07 14:44:49 +0000

    remove the tags from trunk

--- software/LSD_INSTALLER_1_2_0_GA/LSD_INSTALLER_1_1_0_GA/cagwas/WebRoot/helpDocs/caGWAS_Online_Help/wwhdata/common/title.js
+++
@@ -1,4 +0,0 @@
-function  WWHBookData_Title()
-{
-  return "caGWAS Online Help";
-}
--- software/LSD_INSTALLER_1_2_0_GA/LSD_INSTALLER_1_1_0_GA/cagwas/db/db-upgrade/mysql/generisize-participant-081024.sql
+++
@@ -1,4 +0,0 @@
-update study_participant set participant_did = Concat('XYZ',substr(participant_did, 7))
-where participant_did like 'CG%';
-commit;
-
